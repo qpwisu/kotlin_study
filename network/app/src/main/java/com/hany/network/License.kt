f9093ca649dc2282eb4bbb86876ce7c80c12a7f9
qpwisu <qpwisu@gmail.com>
2021-11-15 15:45:06 +0900

    network+retrofit

---
+++ network/app/src/main/java/com/hany/network/License.kt
@@ -0,0 +1,9 @@
+package com.hany.network
+
+data class License(
+    val key: String,
+    val name: String,
+    val node_id: String,
+    val spdx_id: String,
+    val url: String
+)
---
+++ network/app/src/main/java/com/hany/network/Repository.kt
@@ -0,0 +1,3 @@
+package com.hany.network
+
+class Repository : ArrayList<RepositoryItem>()
